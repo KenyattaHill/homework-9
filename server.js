93081675f511102f53e26136b33568d2858630ac
Kenyatta Hill <khill@insight2profit.com>
2020-06-12 09:33:31 -0400

    Issues #3

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'public')))
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'notes.html'))
+})
+
 app.listen(PORT, () => {
   console.log('App is running at http://localhost:3000')
 })
